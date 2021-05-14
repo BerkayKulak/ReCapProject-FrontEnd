440d58833115cfb3f8c63d5f38502c14d7434ac1
Berkay Kulak <beko_468@hotmail.com>
2021-05-15 00:44:44 +0300

    15-05

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarDetailComponent } from './components/car-detail/car-detail.component';
 import { CarComponent } from './components/car/car.component';
+import { RentalComponent } from './components/rental/rental.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: CarComponent },
   { path: 'cars', component: CarComponent },
   { path: 'cars/brand/:brandId', component: CarComponent },
-  {path:"cars/filter/:brandId/:colorId",component:CarComponent},
+  { path: 'cars/filter/:brandId/:colorId', component: CarComponent },
+  { path: 'carrental/:carId ', component: RentalComponent },
   { path: 'cardetail/:carId', component: CarDetailComponent },
   { path: 'cars/color/:colorId', component: CarComponent },
 ];
